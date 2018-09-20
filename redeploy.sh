19e367ab3b44a2161be9f95d1798d96f03a39a82
Robert Masen <r@robertmasen.pizza>
2018-09-20 13:59:17 -0500

    ...

--- redeploy.sh
+++ redeploy.sh
@@ -3,7 +3,7 @@ cargo build --release
 if [ $? -ne 0 ]; then
     exit $?
 fi
-if [ -z "$PID"]; then
+if [ ! -z "$PID"]; then
     kill -kill $PID
 fi
 RUST_LOG=pizza_freak:info,pizza_freak:error ./target/release/pizza_freak > ~/logs/pizza_freak.log &

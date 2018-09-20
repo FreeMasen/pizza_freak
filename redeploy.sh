fa2fa237061713795b4a0c3c723014c7aceb9247
rfm <r.f.masen@gmail.com>
2018-09-20 19:18:44 +0000

    bash...

--- redeploy.sh
+++ redeploy.sh
@@ -3,7 +3,7 @@ cargo build #--release
 if [ $? -ne 0 ]; then
     exit $?
 fi
-if [ -z "$PID"]; then
+if [ -z "$PID" ]; then
     echo no current pizza_freak running
 else
     echo killing old pizza_freak at $PID

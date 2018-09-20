8a8e5f79b04c3a7818c322aa567e744ea91a0ce0
Robert Masen <r@robertmasen.pizza>
2018-09-20 14:14:11 -0500

    ...

--- redeploy.sh
+++ redeploy.sh
@@ -1,9 +1,12 @@
 PID=`getpid pizza_freak`
-cargo build --release
+cargo build #--release
 if [ $? -ne 0 ]; then
     exit $?
 fi
-if [[ ! -z "$PID"]]; then
+if [ -z "$PID"]; then
+    echo no current pizza_freak running
+else
+    echo killing old pizza_freak at $PID
     kill -kill $PID
 fi
 RUST_LOG=pizza_freak:info,pizza_freak:error ./target/release/pizza_freak > ~/logs/pizza_freak.log &
